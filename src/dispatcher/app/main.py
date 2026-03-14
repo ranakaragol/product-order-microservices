09e2855ca211e9b4f31a0bbde31bdb0ca3161ef8
Rana Karagöl <rnakrql@icloud.com>
2026-03-14 22:04:30 +0300

    feat(dispatcher): make auth and route tests pass

--- src/dispatcher/app/main.py
+++ src/dispatcher/app/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
-app = FastAPI()
+app= FastAPI()
+#Dispatcher için ilk yetkilendirme
+@app.middleware("http")
+async def check_auth(request: Request, call_next):
+    #istek "/products" rotasına geliyorsa ve Header'da yetki yoksa..
+    #401 yetkisiz- Unauthorized hatası
+    if request.url.path.startswith("/products"):
+        if "Authorization" not in request.headers:
+            return JSONResponse(status_code=401, content={"error": "Unauthorized"})
+    #Her şey doğruysa veya rota başka bir yerse, isteğin geçmesine izin ver
+    response= await call_next(request)
+    return response
 
 @app.get("/")
-def root():
-    return {"message": "service running"}
+def read_root():
+    return {"message": "Dispatcher Gateway Running"}
