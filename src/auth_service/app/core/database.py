8dbac47c39d170c8f5f1fd8bebb8d5ecfe6074b7
Rana Karagöl <rnakrql@icloud.com>
2026-03-15 21:19:42 +0300

    isolate databases in docker-compose

---
+++ src/auth_service/app/core/database.py
@@ -0,0 +1,12 @@
+import os 
+from motor.motor_asyncio import AsyncIOMotorClient
+
+#Docker üzerinden gelirse mongo_url, yoksa yerel adres kullanılır
+MONGO_URL=os.getenv("MONGO_URL", "mongodb://localhost:27017/auth_db")
+
+#Asenkron mongodb istemcisi oluşturulur ve veritabanı seçilir
+client=AsyncIOMotorClient(MONGO_URL)
+db=client.get_database()
+#Kullanıcıların tutulacağı koleksiyon
+users_collection=db.get_collection("users")
+
