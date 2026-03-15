b5696f8e047864706c0a410778e37716c2150dbc
Rana Karagöl <rnakrql@icloud.com>
2026-03-15 20:11:42 +0300

    save minor changes in dispatcher

--- src/dispatcher/app/main.py
+++ src/dispatcher/app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from .core.security import is_authorized
+
 
 app= FastAPI()
 #Dispatcher için ilk yetkilendirme
 @app.middleware("http")
 async def check_auth(request: Request, call_next):
-    #istek "/products" rotasına geliyorsa ve Header'da yetki yoksa..
-    #401 yetkisiz- Unauthorized hatası
-    if request.url.path.startswith("/products"):
-        if "Authorization" not in request.headers:
-            return JSONResponse(status_code=401, content={"error": "Unauthorized"})
-    #Her şey doğruysa veya rota başka bir yerse, isteğin geçmesine izin ver
+    #Core modülü işlemleri yapar
+    if not is_authorized(request):
+        return JSONResponse(status_code=401, content={"error": "Unauthorized"})
+    
     response= await call_next(request)
     return response
 
