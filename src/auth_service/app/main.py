89921829f9971854261c85383fb6aa5103b90e30
Rana Karagöl <rnakrql@icloud.com>
2026-03-15 20:42:24 +0300

    implement login endpoint to pass tests

--- src/auth_service/app/main.py
+++ src/auth_service/app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "service running"}
+#Kullanıcıdan gelecek giriş bilgilerinin modelini tanımlıyoruz
+class LoginRequest(BaseModel):
+    username:str
+    password:str
+
+
+@app.post("/login")
+def login(request: LoginRequest):
+    #Doğru kullanıcı adı ve şifre
+    if request.username=="admin" and request.password=="password123":
+        return {"access_token": "super-secret-jwt-token", "token_type": "bearer"}
+    #Yanlış bilgide 401 Yetkisiz hatası
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Geçersiz kullanıcı adı ve şifre"
+    )
