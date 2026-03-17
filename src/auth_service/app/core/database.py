57de4f9be1cbc09bc29dcaf24827460c67c56eaa
Rana Karagöl <rnakrql@icloud.com>
2026-03-17 23:28:45 +0300

    correct database typo and update docker-compose

--- src/auth_service/app/core/database.py
+++ src/auth_service/app/core/database.py
@@ -8,5 +8,5 @@ MONGO_URL=os.getenv("MONGO_URL", "mongodb://localhost:27017/auth_db")
 client=AsyncIOMotorClient(MONGO_URL)
 db = client["auth_db"]
 #Kullanıcıların tutulacağı koleksiyon
-users_collection=db.db["users"]
+users_collection=db["users"]
 
