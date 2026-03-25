38acf1a3916dccf61fd4fc03f25dba57a13cf382
Rana Karagöl <rnakrql@icloud.com>
2026-03-25 15:59:59 +0300

    test(order): add failing tests for order crud

---
+++ src/order_service/tests/test_main.py
@@ -0,0 +1,47 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
+from app.main import app
+
+@pytest.mark.asyncio(loop_scope="function")
+async def test_get_orders_returns_200_and_empty_list():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response=await ac.get("/orders")
+
+    assert response.status_code==200
+    assert response.json()==[]
+
+@pytest.mark.asyncio(loop_scope="function")
+async def test_create_order():
+    payload={
+        "product_id": "123",
+        "quantity": 2
+
+    }
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response=await ac.post("/orders", json=payload)
+
+    assert response.status_code==201
+    assert response.json()["product_id"]== "123"
+
+@pytest.mark.asyncio(loop_scope="function")
+async def test_get_order_by_id():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        create=await ac.post("/orders", json={
+            "product_id": "123",
+            "quantity": 1
+        })
+
+        order_id=create.json()["id"]
+        get_res=await ac.get(f"/orders/{order_id}")
+
+    assert get_res.statuc_code==200
+
+
+@pytest.mark.asyncio(loop_scope="function")
+async def test_get_order_invalid_id():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response=await ac.get("/orders/invalid_id")
+
+    assert response.status_code==200
+
